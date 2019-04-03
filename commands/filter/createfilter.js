758acd733d54a68857def684a20b9da6dd8f51db
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-03 17:50:58 +0100

    Update createfilter.js

--- commands/filter/createfilter.js
+++ commands/filter/createfilter.js
@@ -6,10 +6,10 @@ class CreateFilterCommand extends Command {
         let [ name, words ] = message.args; 
         const filters = await message.guild.filters();
         name = name.toLowerCase();
-        if (!!filters[name]) return await reply.fail(`Filter \`${name}\` already exists`);
+        if (!!filters[name]) return await reply.fail(`O filtro \`${name}\` já existe`);
         
         words = words.split(" ").filter(s => s != "").map(s => s.replace(/[^a-z]/gi, '')).map(String.toLowerCase);
-        if (words.length == 0) return await reply.fail("No valid words found.");
+        if (words.length == 0) return await reply.fail("Não foram encontradas palavras válidas.");
 
         filters[name] = {
             words,
@@ -18,23 +18,23 @@ class CreateFilterCommand extends Command {
         }
 
         await message.guild.filters(filters);
-        return await reply.succ("Added filter " + name + ": ", words.join(", "));
+        return await reply.succ("Adicionado filtro " + name + ": ", words.join(", "));
     }
 
     options() { return {
 
     }}
 
-    help = "Create a new content filter.";
+    help = "Cria um novo filtro de conteudo.";
     userPerms = ["MANAGE_GUILD"];
     args = [{
         type: "string",
-        info: "The name of this filter",
+        info: "O nome do filtro",
         max: "100",
         example: "badwords"
     }, {
         type: "string",
-        info: "The words in the filter. Only letters allowed.",
+        info: "As palavras do filtro. Apenas letras.",
         example: "heck frick meme dang shoot"
     }];
 }
