3826ae15899ff1d0789d41ffcd31c1fd832394d0
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-03 18:03:41 +0100

    Update listfilters.js

--- commands/filter/listfilters.js
+++ commands/filter/listfilters.js
@@ -8,25 +8,25 @@ class ListFiltersCommand extends Command {
         const keys = Object.keys(filters);
 
         if (!name) return keys.length > 0 ?
-            await reply.succ("Filters on this server: ", keys.join(", ")) :
-            await reply.warn("No filters exist.");
+            await reply.succ("Filtros neste server: ", keys.join(", ")) :
+            await reply.warn("Não existem filtros.");
 
-        if (!filters[name]) return await reply.fail("Filter `" + name + "` does not exist.");
+        if (!filters[name]) return await reply.fail("Filtro `" + name + "` não existe.");
 
         const w = filters[name].words;
         return w.length == 0 ?
-            await reply.warn("Filter `" + name + "` has no words.") :
-            await reply.succ("The filter `" + name + "` has: ", w.join(", "));
+            await reply.warn("Filtro `" + name + "` não tem palavras.") :
+            await reply.succ("O filtro `" + name + "` tem: ", w.join(", "));
 
     }
 
-    help = "List the filters or words in a filter.";
+    help = "Mostra a lista de palavras em um filtro.";
     usage = "";
     alias = ["filterlist", "wordlist", "listwords"];
     userPerms = ["MANAGE_GUILD"];
     arg = {
         type: "string",
-        info: "The name of a filter",
+        info: "O nome do filtro",
         example: "badwords",
         default: null,
     };
