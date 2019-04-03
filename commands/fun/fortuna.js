667fd00e5bbfc7bb1d1689426994a586b75e23cc
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-03 18:14:11 +0100

    Update and rename fortune.js to fortuna.js

--- commands/fun/fortuna.js
+++ commands/fun/fortuna.js
@@ -6,13 +6,13 @@ class FortuneCommand extends Command {
 
     async run({ message, bot, send, t }) {
         const request = await snekfetch.get("http://fortunecookieapi.herokuapp.com/v1/cookie");
-        if (!request || !request.body || !request.body[0]) return send("**Bad Cookie**")
+        if (!request || !request.body || !request.body[0]) return send("**Biscoito podre**")
         const cookie = request.body[0];
 
         const { fortune, lesson, lotto } = cookie;
         if ((!fortune || !fortune.message) ||
             (!lesson || !lesson.english || !lesson.chinese) ||
-            (!lotto || !lotto.numbers)) return send("**Bad Cookie**")
+            (!lotto || !lotto.numbers)) return send("**Biscoito podre**")
 
         const fm = fortune.message.replace(/\\n|\\r|\\t/g, "");
         const ls = `${lesson.chinese} (${lesson.english})`
@@ -27,7 +27,7 @@ class FortuneCommand extends Command {
         await msg.edit(txt);
     }
 
-    help = "Open a fortune cookie";
+    help = "Abre um biscoito da sorte";
     usage = "{}fortune";
     cooldown = 5;
 }
