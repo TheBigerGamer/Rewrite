5a71f658721b4f9c22208e25f555c9a5c6b95cb4
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-03 17:41:08 +0100

    Update setserverlog.js

--- commands/config/setserverlog.js
+++ commands/config/setserverlog.js
@@ -6,23 +6,23 @@ class SetServerLogCommand extends Command {
         const modlog = await message.guild.serverlog();
         if (modlog) {
             await message.guild.serverlog(false);
-            return await reply.succ("Disabled Server Log");
+            return await reply.succ("Server Log desativado ");
         }
 
         const [channel] = message.args;
         const perms = channel.permissionsFor(bot.user);
         if (!perms.has("SEND_MESSAGES") || !perms.has("EMBED_LINKS"))
-            return await reply.warn("I need permission to send embeds in " + channel.toString());
+            return await reply.warn("Preciso de permissão para enviar embeds em " + channel.toString());
 
         await message.guild.serverlog(channel.id);
-        return await reply.succ("Server log set to " + channel.toString());
+        return await reply.succ("Server log defenido para " + channel.toString());
     }
 
-    help = "Set the channel that server events are logged.";
+    help = "Define o canal onde o Server Log é registado.";
     userPerms = ["MANAGE_GUILD"];
     arg = {
         type: "channel",
-        info: "The channel to send logs",
+        info: "O canal para enviar logs",
         example: "#serverlog",
         default: true
     };
