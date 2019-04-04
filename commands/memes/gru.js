22ed22807c6c571a1279355ef159925c27e217ac
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-04 17:10:59 +0100

    Update gru.js

--- commands/memes/gru.js
+++ commands/memes/gru.js
@@ -5,7 +5,7 @@ class GruCommand extends Command {
     async run({ message, bot, reply, t }) {
         const [i] = message.args;
         const txt = util.delimiter(i, '|', 3);
-        if (!txt) return reply.fail("Minimum of 3 texts.")
+        if (!txt) return reply.fail("Minimo de 3 textos.")
         const gru = Image.getStaticFile("gru");
         const opts = [160, 218, "arialblack.ttf", 20, "black", "northwest"];
         const cap1 = await Image.createCaption(txt[0], ...opts);
@@ -20,12 +20,12 @@ class GruCommand extends Command {
         return reply(Image.send(buf));
     }
 
-    help = "The gru meme.";
+    help = "O meme do gru.";
     botPerm = "ATTACH_FILES";
     arg = {
         type: "string",
-        info: "The text delimited by `|`",
-        example: "Find a gru meme template | Create a custom gru meme generator | Nobody uses it"
+        info: "O texto é delimitado por `|`",
+        example: "Encontra um exemplo do gru | Cria um gerador do meme do gru | Ninguém o usa"
     };
     cooldown = 5;
 }
