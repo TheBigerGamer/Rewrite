f9d8c097cec4f4741746f8bfa26dc32c771a7642
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-04 17:47:58 +0100

    Update softban.js

--- commands/mod/softban.js
+++ commands/mod/softban.js
@@ -4,24 +4,24 @@ class BanCommand extends Command {
 
     async run({ message, bot, reply, t }) {
         const [member, reason] = message.args;
-        if (!member.bannable) return await reply.fail("I lack permission to softban this user.");
-        if (message.member.roles.highest.position <= member.roles.highest.position) return await reply.fail("You lack permission to softban this user.");
+        if (!member.bannable) return await reply.fail("Não tenho permissões para banir este utilizador.");
+        if (message.member.roles.highest.position <= member.roles.highest.position) return await reply.fail("Não tens permissões para softbanar este utilizador.");
 
-        let txt = `Are you sure you want to softban the user ${member.user.tag}`;
+        let txt = `Tens a certeza de que queres banir suavemente este utilizador ${member.user.tag}`;
         const m = await message.channel.ask(message.author, txt);
         const failsafe = await message.channel.collectMessage(message.author);
-        if (!failsafe) return await m.edit("**Aborted.**", { embed: null });
-        else await m.edit(`**Softbanning...**`, { embed: null });
+        if (!failsafe) return await m.edit("**Abortado.**", { embed: null });
+        else await m.edit(`**Softbanindo...**`, { embed: null });
         try {
             await member.ban({ reason, days: 7 });
-            await m.edit("**Unbanning**")
+            await m.edit("**Desbanindo**")
             await member.unban({ reason: "Softban" });
         } catch (e) {
-            return await m.edit("**Softban failed**");
+            return await m.edit("**Softban falhou**");
         }
         await message.guild.userAction(member.user.id, "softban");
         await message.guild.modAction(message.author.id, "softban");
-        await m.edit("**Softban complete**");
+        await m.edit("**Softban completo**");
 
         const modlogID = await message.guild.modlog();
         const modlog = bot.channels.get(modlogID);
@@ -33,18 +33,18 @@ class BanCommand extends Command {
         });
     }
 
-    help = "Ban and unban a member to delete their messages.";
+    help = "Bane e unbane um utilizador para eliminar as suas mensagens.";
     alias = ["softbanne", "softb&"];
     botPerms = ["BAN_MEMBERS"];
     userPerms = ["BAN_MEMBERS"];
     args = [{
         type: "member",
-        info: "The user to softban.",
+        info: "O utilizador para banir suavemente.",
         example: "@Badboy"
     }, {
         type: "string",
-        info: "The reason for softbanning.",
-        example: "Spamming in chat.",
+        info: "A razão do banimento suave.",
+        example: "Alguma coisa.",
         default: "unspecified"
     }];
 }
