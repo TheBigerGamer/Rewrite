e57db561f7b56fcc32ef2a195906941367404871
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-04 18:24:55 +0100

    Update playingchan.js

--- commands/music/playingchan.js
+++ commands/music/playingchan.js
@@ -4,24 +4,24 @@ class PlayingChannelCommand extends Command {
 
     async run ({message, bot, reply, t}) {
         const vc = message.member.voiceChannel;
-        if (!vc) return reply.warn("Join a voice channel to run this command.");
+        if (!vc) return reply.warn("Entra num canal de voz para ouvires música.");
 
         const player = bot.player.check(vc);
-        if (!player) return reply.warn("There is no music playing.")
+        if (!player) return reply.warn("Não há música a tocar.")
 
         if (!player.isDJ(message.author.id, message.channel)) 
-            return reply.warn("You need host permissions to run this command.");
+            return reply.warn("Precisas de permissões de host para executar este comando.");
 
         const [chan] = message.args;
         player.setBroadcast(chan.id);
-        return reply.succ("Now playing channel set to " + chan);
+        return reply.succ("Canal **Agora no ar** definido para " + chan);
     }
 
-    help = "Set the channel now playing messages are sent in.";
+    help = "Define o canal onde as mensagens de **Agora no ar** são enviadas.";
     alias = "playingchannel"
     arg = {
         type: "channel",
-        info: "The channel to set.",
+        info: "O canal para as mensagens.",
         example: "#music",
         default: true
     }
