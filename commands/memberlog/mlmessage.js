a86334710cbe3b73413b0546747881f9ee0f8add
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-04 17:07:44 +0100

    Update mlmessage.js

--- commands/memberlog/mlmessage.js
+++ commands/memberlog/mlmessage.js
@@ -5,34 +5,34 @@ class MLMessageCommand extends Command {
     async run ({message, bot, reply, t}) {
         const [ when, type, msg ] = message.args;
 
-        if (type === "disable") {
+        if (type === "desativar") {
             await message.guild[`ml${when}`](false);
-            return await reply.succ("Disabled `" + when +"` messages.");
+            return await reply.succ("Desativadas `" + when +"` mensagens.");
         }
 
-        if (type === "text" && !msg) return await reply.fail("Type text needs a message.");
+        if (type === "text" && !msg) return await reply.fail("Escrever texto necessita de uma mensagem.");
 
         await message.guild[`ml${when}`]({ type, msg });
-        return await reply.succ(when + " message set.");
+        return await reply.succ(when + " definida mensagem.");
     }
 
-    help = "Set the memberlog join or leave message and type";
+    help = "Define o log de membros para entrar ou sair e a mensagem";
     alias = "mlmsg";
     userPerms = ["MANAGE_GUILD"];
     args = [{
         type: "selection",
         items: ["join", "leave"],
-        info: "For the `join` or `leave` message.",
+        info: "Para as mensagens de `entrar` ou `sair`.",
         example: "join"
     }, {
         type: "selection",
         items: ["text", "embed", "image", "disable"],
-        info: "The type of message to send. `text`, `embed`, `image`, or `disable` to turn off.",
+        info: "O tipo de mensagem para enviar. `text`, `embed`, `image`, ou `desativar` para desligar.",
         example: "embed"
     }, {
         type: "string",
-        info: "The text sent in the message.",
-        example: "Welcome {name} to HQ.",
+        info: "O texto da mensagem.",
+        example: "Bem-vindo {name} ao HQ.",
         default: null
     }];
 }
