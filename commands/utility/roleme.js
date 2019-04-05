3667c8cce2af7d7fc6556e002edf91e2c5f34c81
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-05 16:52:25 +0100

    Update roleme.js

--- commands/utility/roleme.js
+++ commands/utility/roleme.js
@@ -5,30 +5,30 @@ class RoleMeCommand extends Command {
     async run({ message, bot, reply, t }) {
         const [role] = message.args;
         const roles = await message.guild.rolemeroles();
-        if (!roles.includes(role.id)) return reply.fail(`\`${role.name}\` is not assignable.`);
+        if (!roles.includes(role.id)) return reply.fail(`\`${role.name}\` não é atribuível.`);
         if (!message.member.roles.has(role.id)) {
             try {
                 await message.member.roles.add(role);
-                return reply.succ(`Assigned \`${role.name}\``);
+                return reply.succ(`Atribuído \`${role.name}\``);
             } catch {
-                return reply.fail(`I don't have permission to assign \`${role.name}\`.`);
+                return reply.fail(`Não tenho permissões para atribuír \`${role.name}\`.`);
             }
         } else {
             try {
                 await message.member.roles.remove(role);
-                return reply.succ(`Removed \`${role.name}\``);
+                return reply.succ(`Removido \`${role.name}\``);
             } catch {
-                return reply.fail(`I don't have permissions to remove \`${role.name}\``);
+                return reply.fail(`Não tenho permissões para remover \`${role.name}\``);
             }
         }
     }
 
-    help = "Assign yourself a role.";
+    help = "Atribui um cargo a ti mesmo.";
     alias = "iam";
     arg = {
         type: "role",
-        info: "The role to assign.",
-        example: "User"
+        info: "O cargo para atribuir.",
+        example: "Membro"
     }
 }
 
