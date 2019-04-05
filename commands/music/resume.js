7587e87073b9f0299245bdfd37b51d1cd2123db8
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-05 16:20:13 +0100

    Update resume.js

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -4,19 +4,19 @@ class ResumeCommand extends Command {
 
     async run ({message, bot, reply, t}) {
         const vc = message.member.voiceChannel;
-        if (!vc) return reply.warn("Join a voice channel to run this command.");
+        if (!vc) return reply.warn("Entra num canal de voz para executares este comando.");
 
         const player = bot.player.check(vc);
-        if (!player) return reply.warn("There is no music playing.")
+        if (!player) return reply.warn("Não há musica a tocar.")
 
         if (!player.isDJ(message.author.id, message.channel)) 
-            return reply.warn("You need host permissions to run this command.");
+            return reply.warn("Precisas de permissões de host para usares este comando.");
 
         player.unpause();
-        return reply.succ("Music unpaused.");
+        return reply.succ("Música a tocar novamente.");
     }
 
-    help = "Resume paused music.";
+    help = "Põe a tocar a musica pausada.";
 }
 
 module.exports = ResumeCommand;
