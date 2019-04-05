60a13ad53e7f181e369214944b575a34e729f05f
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-05 16:28:47 +0100

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -3,19 +3,19 @@ const { Command } = require("../../Nitro");
 class StopMusicCommand extends Command {
     async run ({message, bot, reply, t}) {
         const vc = message.member.voiceChannel;
-        if (!vc) return reply.warn("Join a voice channel to run this command.");
+        if (!vc) return reply.warn("Entra num canal de voz para executares este comando.");
 
         const player = bot.player.check(vc);
-        if (!player) return reply.warn("There is no music playing.")
+        if (!player) return reply.warn("Não há música a tocar.")
 
         if (!player.isDJ(message.author.id, message.channel)) 
-            return reply.warn("You need DJ permissions to run this command.");
+            return reply.warn("Precisas de ser DJ para executar este comando.");
 
         player.kill();
-        return reply.succ("Music stopped.");
+        return reply.succ("Música parada.");
     }
 
-    help = "Stop currently playing music.";
+    help = "Para a reprodução de música.";
 }
 
 module.exports = StopMusicCommand;
