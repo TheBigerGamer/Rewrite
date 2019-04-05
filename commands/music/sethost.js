7cd4bcff8224ca6fb4fd780c29d39abd99c26d10
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-05 16:22:27 +0100

    Update sethost.js

--- commands/music/sethost.js
+++ commands/music/sethost.js
@@ -4,23 +4,23 @@ class SetHostCommand extends Command {
 
     async run ({message, bot, reply, t}) {
         const vc = message.member.voiceChannel;
-        if (!vc) return reply.warn("Join a voice channel to run this command.");
+        if (!vc) return reply.warn("Entra num canal de voz paras executares este comando.");
 
         const player = bot.player.check(vc);
-        if (!player) return reply.warn("There is no music playing.")
+        if (!player) return reply.warn("Não há musica a tocar.")
 
         if (!player.isDJ(message.author.id, message.channel)) 
-            return reply.warn("You need host permissions to run this command.");
+            return reply.warn("Precisas de permissões de host para executares este comando.");
 
         const [user] = message.args;
         player.setDJ(user.id);
-        return reply.succ(`${user.tag} has been made the host.`);
+        return reply.succ(`${user.tag} foi feito host.`);
     }
 
-    help = "Set the host of the current session.";
+    help = "Define o host da sessão atual.";
     arg = {
         type: "user",
-        info: "The new host to set.",
+        info: "O utilizador para ser o host.",
         example: "@otheruser"
     }
 }
