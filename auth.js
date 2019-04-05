dd0732746706b9baefec6a64a78485a4088a3f1a
TheBigerGamer Oficial <39341546+TheBigerGamer@users.noreply.github.com>
2019-04-05 17:23:57 +0100

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,29 @@
+module.exports = {
+    TOKEN: "NTQ4NjI5MjQyMzM0ODA2MDI5.XKeBBQ.ISHyiYFFlZE3Lg25i0DsV0-JXK0", // Bot token
+
+    SENTRY: "", // Sentry url for error notification
+
+    DBNAME: "Nitro",
+    DBPASS: "", // Not used
+    DBDIR: "", // Not used
+
+    DIS_MUSIC: false, // Disable music and the requiment for lavalink
+
+    DBL_PASS: "", // The discordbotlist webhook server
+    DBL_PORT: "",
+
+    SHARDS: 4, // Dosnt seem to do anything
+
+    LAVALINK: "", // Lavalink password defined in application.yaml
+
+    LISTS: { // Just ignore
+        botsdiscordpw: "",
+        discordbots: "",
+        carbonitex: "",
+        novo: ""
+    },
+
+    BETA: true, // Not used yet
+
+    YTAPITOKEN: "" // google api token
+}
